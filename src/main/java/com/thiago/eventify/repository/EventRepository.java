2af5c1f992cb074a78662677ecff40eef09f2f32
Thiago Bueno <thiagobuenogoncalves@gmail.com>
2025-03-26 12:26:22 -0300

    Criar UserRepository e EventRepository

---
+++ src/main/java/com/thiago/eventify/repository/EventRepository.java
@@ -0,0 +1,11 @@
+package com.thiago.eventify.repository;
+
+import com.thiago.eventify.entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, UUID> {
+}
---
+++ src/main/java/com/thiago/eventify/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.thiago.eventify.repository;
+
+import com.thiago.eventify.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}

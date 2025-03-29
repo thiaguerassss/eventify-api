a104ba02c94448b96d4eed4a1446743a900a54fc
Thiago Bueno <thiagobuenogoncalves@gmail.com>
2025-03-29 00:14:00 -0300

    Implementar método no EventMapper

--- src/main/java/com/thiago/eventify/mapper/EventMapper.java
+++ src/main/java/com/thiago/eventify/mapper/EventMapper.java
@@ -2,7 +2,10 @@ package com.thiago.eventify.mapper;
 
 import com.thiago.eventify.dto.CreateEventDTO;
 import com.thiago.eventify.dto.EventInListDTO;
+import com.thiago.eventify.dto.UpdateEventDTO;
 import com.thiago.eventify.entity.Event;
+
+import java.util.Objects;
 
 public class EventMapper {
 
@@ -16,6 +19,14 @@ public class EventMapper {
         event.setCep(data.cep());
         event.setAddressNumber(data.addressNumber());
         return event;
+    }
+
+    public static void updateEntity(UpdateEventDTO data, Event event){
+        if (Objects.nonNull(data.title())) event.setTitle(data.title());
+        if (Objects.nonNull(data.description())) event.setDescription(data.description());
+        if (Objects.nonNull(data.dateTime())) event.setDateTime(data.dateTime());
+        if (Objects.nonNull(data.cep())) event.setCep(data.cep());
+        if (Objects.nonNull(data.addressNumber())) event.setAddressNumber(data.addressNumber());
     }
 
     public static EventInListDTO toDTO(Event event){

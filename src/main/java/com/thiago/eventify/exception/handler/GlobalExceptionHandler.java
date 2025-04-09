3ca2e55067fe80dbddddb1e8e89f4161bdb37880
Thiago Bueno <thiagobuenogoncalves@gmail.com>
2025-04-09 20:26:15 -0300

    Implementar novo método ExceptionHandler

--- src/main/java/com/thiago/eventify/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/thiago/eventify/exception/handler/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.hibernate.ObjectNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -53,6 +54,12 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleFeignError(FeignException ex, HttpServletRequest req) {
         String message = "Erro ao acessar serviço externo: " + ex.getMessage();
         return buildErrorResponse(HttpStatus.BAD_GATEWAY, message, req);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorResponse> handleDataIntegrityViolation(DataIntegrityViolationException ex,
+                                                                      HttpServletRequest req){
+        return buildErrorResponse(HttpStatus.CONFLICT, "Ocorreu um erro de integridade de dados.", req);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)

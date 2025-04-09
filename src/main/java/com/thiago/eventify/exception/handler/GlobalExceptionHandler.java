c1d86457da99962fb403285108683fb7ef4c7254
Thiago Bueno <thiagobuenogoncalves@gmail.com>
2025-04-09 04:12:47 -0300

    Refatorar GlobalExceptionHandler

--- src/main/java/com/thiago/eventify/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/thiago/eventify/exception/handler/GlobalExceptionHandler.java
@@ -13,13 +13,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @ControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
